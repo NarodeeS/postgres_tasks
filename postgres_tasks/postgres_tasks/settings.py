5b4890324e1616cba08552e5b088ebf46407ddd1
George Stykalin <gstykalin@mail.ru>
2023-04-12 15:51:41 +0300

    update nginx

--- postgres_tasks/postgres_tasks/settings.py
+++ postgres_tasks/postgres_tasks/settings.py
@@ -56,6 +56,8 @@ ROOT_URLCONF = "postgres_tasks.urls"
 #     "http://localhost:8000",
 #     "http://localhost:8080",
 # ]
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'https://localhost']
+
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
@@ -123,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
