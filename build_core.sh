97790c4255b8d1497bffb6de5615524a96fd6cc3
George Stykalin <gstykalin@mail.ru>
2023-05-17 16:59:16 +0300

    update building images and ci

---
+++ build_core.sh
@@ -0,0 +1,7 @@
+export DOCKER_BUILDKIT=0
+export COMPOSE_DOCKER_CLI_BUILD=0
+
+docker-compose -f ./base-compose.yml \
+    -f ./postgres_tasks/docker-compose.yml \
+    -f ./postgres_front/docker-compose.yml \
+    build
--- start_core.sh
+++ start_core.sh
@@ -1,5 +1,4 @@
 docker-compose -f ./base-compose.yml \
     -f ./postgres_tasks/docker-compose.yml \
     -f ./postgres_front/docker-compose.yml \
-    -f ./monitoring/docker-compose.yml \
     up -d
